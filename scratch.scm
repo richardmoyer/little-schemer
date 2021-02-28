1d8ffff59cfb17dce4b3f2e3f22387b2d54d8db9
Richard Moyer <richmoyer@gmail.com>
2021-02-28 13:01:04 -0600

    notes

---
+++ scratch.scm
@@ -0,0 +1,21 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(defun atom? (x)
+  (not (listp x)))
+
+(atom? 1)
+
+;; lat?
+(define lat?
+  (lambda (l)
+    (cond((null? l) #t)
+         ((atom? (car l)) (lat? (cdr l)))
+         (else #f))))
+
+
+(defun lat?
+  (lambda (l) #t)
+  ((atom? (car l) (lat? (cdr l)))
+   (else #f)))
